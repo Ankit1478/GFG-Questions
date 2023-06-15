ad8f66cb95f8d1bef9f6da77f858b31d6409bfb7
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-06-16 00:22:04 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] s = br.readLine().trim().split(" ");
+            int[][] Intervals = new int[n][2];
+            int j = 0;
+            for(int i = 0; i < n; i++){
+                Intervals[i][0] = Integer.parseInt(s[j]);
+                j++;
+                Intervals[i][1] = Integer.parseInt(s[j]);
+                j++;
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.overlappedInterval(Intervals);
+            for(int i = 0; i < ans.length; i++){
+                for(j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int[][] overlappedInterval(int[][] intervals)
+    {
+        // Code here // Code here
+        int n = intervals.length; // size of the array
+        // sort the given intervals:
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            // if the current interval does not
+            // lie in the last interval:
+            if (ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(intervals[i][0], intervals[i][1]));
+            }
+            // if the current interval
+            // lies in the last interval:
+            else {
+                ans.get(ans.size() - 1).set(1, Math.max(ans.get(ans.size() - 1).get(1), intervals[i][1]));
+            }
+        }
+        
+        // Convert the ArrayList to 2D array
+        int[][] result = new int[ans.size()][2];
+        for (int i = 0; i < ans.size(); i++) {
+            result[i][0] = ans.get(i).get(0);
+            result[i][1] = ans.get(i).get(1);
+        }
+        
+        return result;
+    }
+}
