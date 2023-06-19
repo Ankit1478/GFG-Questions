9e9260b96f1c0cb46e33d7dfa85a4c44f3172968
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-06-19 16:49:39 +0530

    Added solution - LeetHub

---
+++ Outermost Parentheses - GFG/outermost-parentheses.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String s;
+            s = br.readLine();
+
+            Solution obj = new Solution();
+            String res = obj.removeOuter(s);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static String removeOuter(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (!stack.isEmpty()) {
+                    sb.append(c);
+                }
+                stack.push(c);
+            } else if (c == ')') {
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    sb.append(c);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
