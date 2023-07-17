1f4e41fff382bfb0deaef3d0abe1b5946183d823
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-07-17 22:04:40 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if (low < high) {
+            int partition = partition(arr, low, high);
+            quickSort(arr, low, partition - 1);
+            quickSort(arr, partition + 1, high);
+        }
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            if (arr[i] <= pivot ) {
+                i++;
+            } else if (arr[j] > pivot) {
+                j--;
+            } else {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+        swap(arr, low, j);
+        return j;
+    } 
+     private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
