a6986c7a3e0c51aad332c01156f530276ac287f6
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-08-29 15:59:58 +0530

    Added solution - LeetHub

---
+++ Subset II - GFG/subset-ii.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter ot = new PrintWriter(System.out);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            int n;
+            n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> ans = obj.printUniqueSubsets(nums);
+            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
+                @Override   public int compare(ArrayList<Integer> a,
+                                             ArrayList<Integer> b) {
+                    int al = a.size();
+                    int bl = b.size();
+                    int min = Math.min(al, bl);
+                    for (int i = 0; i < min; i++) {
+                        if (a.get(i) < b.get(i))
+                            return -1;
+                        else if (a.get(i) > b.get(i))
+                            return 1;
+                    }
+                    if (al < bl)
+                        return -1;
+                    else if (al > bl)
+                        return 1;
+                    return -1;
+                }
+            });
+            ot.print("[ ");
+            for (int i = 0; i < ans.size(); i++) {
+                ot.print("[ ");
+                for (int j = 0; j < ans.get(i).size(); j++)
+                    ot.print(ans.get(i).get(j) + " ");
+                ot.print("]");
+            }
+            ot.println(" ]");
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> printUniqueSubsets(int[] nums) {
+      Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+        findsubset(0,nums,new ArrayList<>(),ans);
+        return ans;
+    }
+    
+    public static void findsubset(int idx,int []nums,ArrayList<Integer>ds,ArrayList<ArrayList<Integer>>ans){
+        if(ans.contains(ds)) return ;
+        ans.add(new ArrayList<>(ds));
+        for(int i=idx;i<nums.length;i++){
+            ds.add(nums[i]);
+            findsubset(i+1,nums,ds,ans);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
